b68128af59a1c1a3cf69bb3d0dd0a092dff15f71
YeeChuen <tychuen98@gmail.com>
2024-03-15 22:00:41 -0500

    required files

---
+++ build_ef_data.py
@@ -0,0 +1,48 @@
+# Author: Yee Chuen Teoh
+# python3 build_ef_data.py
+
+def getFileContent(file_name):
+    with open(file_name, 'r') as f:
+        contents = f.readlines()
+        contents = [x.replace("\n", "") for x in contents]
+        return contents
+
+def removeElement(given_list, to_remove):
+    while to_remove in given_list:
+        given_list.remove(to_remove)
+
+proteins = getFileContent("proteins.txt")
+ef_locations = getFileContent("EFHands_locations_result_list.txt")
+removeElement(proteins, "")
+removeElement(ef_locations, "")
+
+print(proteins[:6])
+print(ef_locations[:6])
+ef_locations = [eval(x) for x in ef_locations]
+
+result = []
+for i in range(len(ef_locations)):
+    idx_p = i * 2
+    seq = proteins[idx_p + 1]
+    ef_num = 1
+    for tuple in ef_locations[i]:
+        motif_type = ef_locations[i][tuple][0]
+
+        if len(seq[tuple[0]:tuple[1]]) != 12:
+            print(f"EF hand not length of 12: {seq[tuple[0]:tuple[1]]}")
+            print(tuple)
+            print(ef_locations[i][tuple])
+            print(proteins[idx_p])
+            print(proteins[idx_p + 1])
+
+        result.append(f"{proteins[idx_p]}_EF{ef_num}|{motif_type}")
+        result.append(seq[tuple[0]:tuple[1]])
+
+        ef_num += 1
+        
+with open("EFhand.fasta", "w") as f:
+    for i, x in enumerate(result):
+        if i % 2 == 0:
+            f.write(f">{x}\n")
+        else:
+            f.write(f"{x}\n")
---
+++ build_ef_struct_data.py
@@ -0,0 +1,99 @@
+# Author: Yee Chuen Teoh
+# python3 build_ef_data.py
+from zipfile import ZipFile
+import os
+
+def extractZip(example_zip_file):
+    with ZipFile(example_zip_file, 'r') as zip: 
+        # printing all the contents of the zip file 
+        zip_dir_list = zip.namelist()
+
+        file_to_extract = []
+        for file in zip_dir_list:
+            if 'rank_005' in file and '.pdb' in file:
+                file_to_extract.append(file)
+        if len(file_to_extract) != 1:
+            print("This zip file has more than 1 rank 005 pdb file.")
+
+        file_extract = file_to_extract[0]
+        zip.extract(file_extract)
+
+example_zip_file = "/Users/Chuen/Documents/vsc projects/lanmodulin_project/_batches/results/batch00/ACA18767-1.result.zip"
+
+zip_directory = "/Users/Chuen/Documents/vsc projects/lanmodulin_project/_batches/results"
+batches = os.listdir(zip_directory)
+batches.sort(key = lambda x: int(x[-2:]))
+
+if not os.path.exists("/Users/Chuen/Documents/vsc projects/lanmodulin_project/pdbs"):
+    os.mkdir("/Users/Chuen/Documents/vsc projects/lanmodulin_project/pdbs")
+os.chdir("/Users/Chuen/Documents/vsc projects/lanmodulin_project/pdbs")
+
+total = 0
+for batch in batches:
+    batch_list_dir = os.listdir(f"{zip_directory}/{batch}")
+
+    zip_list = []
+    for f in batch_list_dir:
+        if ".zip" in f:
+            zip_list.append(f)
+    
+    total += len(zip_list)
+    print(len(zip_list), batch)
+    for z in zip_list:
+        if not os.path.exists(f"{zip_directory}/{batch}/{z}"):
+            extractZip(f"{zip_directory}/{batch}/{z}") # <-- only extract if it does not exist
+
+# extract EF hand.
+pdb_path = "/Users/Chuen/Documents/vsc projects/lanmodulin_project/pdbs"
+pdb_list = os.listdir(pdb_path)
+pdb_list = [f"{pdb_path}/{p}" for p in pdb_list]
+
+def getEFHandPdb(pdb_file, ef_list):
+    with open(pdb_file, 'r') as f:
+        contents = f.readlines()
+        #contents = [x.replace("\n", "") for x in contents]
+
+        ef_hands_pdbs = []
+        ef = []
+        ef_idx = 0
+        print(ef_list[ef_idx][0], ef_list[ef_idx][1])
+        for line in contents:
+            type = line[:4]
+            chain = line[21:22] # <-- we can ignore chain information since it is not needed
+            position = (line[22:26].replace(" ",""))
+            if type == "ATOM":
+                position = int(position)
+                if ef_list[ef_idx][0] + 1 <= position and position <= ef_list[ef_idx][1] + 1:
+                    #print(position)
+                    ef.append(line)
+
+                if position > ef_list[ef_idx][1] + 1:
+                    ef_hands_pdbs.append(ef)
+                    ef = []
+                    ef_idx += 1
+                    if ef_idx >= len(ef_list):
+                        break
+
+        pdb_name = pdb_file.split("/")[-1][:-4]
+        for e in range(len(ef_hands_pdbs)):
+            ef = ef_hands_pdbs[e]
+            with open(f"{pdb_name}_EF{e + 1}.pdb", "w") as f:
+                for line in ef:
+                    f.write(line)
+            
+amino_acid_codes = {
+    "ALA": "A","ARG": "R","ASN": "N","ASP": "D","CYS": "C",
+    "GLN": "Q","GLU": "E","GLY": "G","HIS": "H","ILE": "I",
+    "LEU": "L","LYS": "K","MET": "M","PHE": "F","PRO": "P",
+    "SER": "S","THR": "T","TRP": "W","TYR": "Y","VAL": "V"
+}
+
+if not os.path.exists("/Users/Chuen/Documents/vsc projects/lanmodulin_project/ef_pdbs"):
+    os.mkdir("/Users/Chuen/Documents/vsc projects/lanmodulin_project/ef_pdbs")
+os.chdir("/Users/Chuen/Documents/vsc projects/lanmodulin_project/ef_pdbs")
+
+print(len(amino_acid_codes))
+ef_list = [(11, 23), (35, 47), (60, 72), (84, 96)] # <-- TODO: change this.
+
+for i in range(len(pdb_list)):
+    getEFHandPdb(pdb_list[i], ef_list)
